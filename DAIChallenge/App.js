d449d5f7b1bf0e28b2f9fd95498a58f5a3da003a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-28 10:18:40 -0300

    rehicimos la api request en el app.js, salta error por falta de key/por data no inicializandose bien

--- DAIChallenge/App.js
+++ DAIChallenge/App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Request } from './src/components/Requests';
-import { Card } from './src/components/Card';
+import { React, useState, useEffect } from 'react';
 
 
 export default function App() {
+  const getPlatos = async () => {
+    const resp = await fetch(`${'https://api.spoonacular.com/food/products/search?query=yogurt&apiKey='}${'e7b8481d81cc4dfbb047e0955e432611'}`);
+    const data = await resp.json();
+    return data;
+    }
+
+    const [platos, setPlatos] = useState();
+
+    const traerPlatos = async () => {
+        const data = await getPlatos();
+        console.log(data);
+        setPlatos([data]);
+        };
+
+      useEffect(() => {
+        traerPlatos();
+        console.log(platos);
+      }, []);
+
   return (
     <View style={styles.container}>
-     <Request/>
+    {platos && 
+    platos.map((plato) => (<div><p>{plato.title}</p></div>))}
     </View>
   );
 }
